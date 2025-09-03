34c371bfd07a5b895dc1f9c29e4460d4f0f47fbb
Nguyễn Đức Tây <nguyenductay121999@gmail.com>
2025-09-03 11:05:18 +0700

    03/09/25

--- cJAVA_object-oriented-programming-features/src/com/example/part1AccessEncapsulationAndScope/ver1ScopeAndPublicKeyword/Bank.java
+++ cJAVA_object-oriented-programming-features/src/com/example/part1AccessEncapsulationAndScope/ver1ScopeAndPublicKeyword/Bank.java
@@ -1,4 +1,4 @@
-package com.example.part1AccessEncapsulationAndScope.ver0Codecademy;
+package com.example.part1AccessEncapsulationAndScope.ver1ScopeAndPublicKeyword;
 
 import com.example.part1AccessEncapsulationAndScope.ver0Codecademy.CheckingAccount;
 
@@ -8,7 +8,12 @@ public class Bank{
 
     CheckingAccount accountOne = new CheckingAccount("Zeus", 100, "1");
     CheckingAccount accountTwo = new CheckingAccount("Hades", 200, "2");
+
+    CheckingAccount myBalance = new CheckingAccount("Tae", 500, "5");
+    myBalance.printBalance();
+
     System.out.println(accountOne.name);
+
     accountOne.addFunds(5);
     accountOne.getInfo();
     
--- cJAVA_object-oriented-programming-features/src/com/example/part1AccessEncapsulationAndScope/ver1ScopeAndPublicKeyword/CheckingAccount.java
+++ cJAVA_object-oriented-programming-features/src/com/example/part1AccessEncapsulationAndScope/ver1ScopeAndPublicKeyword/CheckingAccount.java
@@ -1,29 +1,39 @@
-package com.example.part1AccessEncapsulationAndScope.ver0Codecademy;
+package com.example.part1AccessEncapsulationAndScope.ver1ScopeAndPublicKeyword;
 
 public class CheckingAccount{
   
   /*
-  private String name;
+  private String id, name;
   private int balance;
-  private String id;
   */
 
-  public String name;
+  public String id, name;
   public int balance;
-  public String id;
   
-  public CheckingAccount(String inputName, int inputBalance, String inputId){
+  public CheckingAccount(String inputId, String inputName, int inputBalance){
+    
+    id = inputId;
     name = inputName;
     balance = inputBalance;
-    id = inputId;
+
   }
   
   public void addFunds(int fundsToAdd){
+
     balance += fundsToAdd;
+
   }
   
+  public void printBalance(){
+
+    System.out.println("Your balance is " + balance);
+
+  }
+
   public void getInfo(){
+
     System.out.println("This checking account belongs to " + name +". It has " + balance + " dollars in it.");
+    
   }
   
 }
