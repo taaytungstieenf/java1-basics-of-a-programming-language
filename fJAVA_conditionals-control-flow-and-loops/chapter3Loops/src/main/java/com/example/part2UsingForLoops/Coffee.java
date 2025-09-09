aa6d3b3fa90907113a80f7ce6bc293c99979676f
Nguyễn Đức Tây <nguyenductay121999@gmail.com>
2025-09-09 07:18:24 +0700

    09/09/25

---
+++ fJAVA_conditionals-control-flow-and-loops/chapter3Loops/src/main/java/com/example/part2UsingForLoops/Coffee.java
@@ -0,0 +1,15 @@
+package com.example.part2UsingForLoops;
+
+class Coffee {
+  
+  public static void main(String[] args) {
+    
+    for (int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++) {
+      
+      System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+      
+    }
+    
+  }
+  
+}
